92e1113c02fccabd00e75f5272b7ecf9a6687f5a
id0ntc4re <parsikovevgenij470@gmail.com>
2025-11-29 02:28:53 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import "./main.scss";
+import Hero from "../../components/Home/Hero/Hero";
+import Poster from "../../components/Home/Poster/Poster";
+import Preview from "../../components/Home/Preview/Preview";
+import About from "../../components/Home/About/About";
+import New from "../../components/Home/New/New";
+import Exclusive from "../../components/Home/Exclusive/Exclusive";
+import Reviews from "../../components/Home/Reviews/Reviews";
+
+export const metadata = {
+  title: "Купить IQOS Iluma и Terea в Москве | Iluma Store",
+  description:
+    "Оригинальные устройства IQOS Iluma и стики Terea с доставкой по РФ. Гарантия подлинности, низкие цены и акции.",
+  alternates: {
+    canonical: `https://iluma-store.ru`,
+  },
+  openGraph: {
+    title: `Купить IQOS Iluma и Terea в Москве | Iluma Store`,
+    description: `Оригинальные устройства IQOS Iluma и стики Terea с доставкой по РФ. Гарантия подлинности, низкие цены и акции.`,
+    url: `https://iluma-store.ru`,
+    images: [
+      {
+        url: `/favicon/web-app-manifest-512x512`,
+        alt: `Ilumastore`,
+      },
+    ],
+  },
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="hidden-h1">
+        IQOS Iluma и стики Terea недорого и с доставкой по всей России
+      </h1>
+      <Hero />
+      <Poster />
+      <Preview />
+      <Exclusive />
+      <New />
+      <About />
+      <Reviews />
+    </>
+  );
+}
